838a6c1d01b1ffa1cfd11713c78644b7fd963c8b
Vusal Osmanov <115807599+vusalosmanov@users.noreply.github.com>
2023-10-30 16:52:34 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes  , Route} from "react-router-dom";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <>
+    <Main/>
+      <Routes>
+      <Route path=""/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
